070475e2f178cc68052e2cf5096ebd4fed50ab2d
JoaoGuedes <joaoguedes.cjp@gmail.com>
2024-02-17 17:30:02 +0000

    updates to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
-import os
 from setuptools import setup, find_packages
+
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\\n" + fh.read()
 
 setup(
     name='guedesmoney',
@@ -7,6 +14,7 @@ setup(
     author_email="joaoguedes.cjp@gmail.com",
     description="A CLI interface for interacting with Portal das Finanças automatically and programmatically",
     keywords="Portal_Das_Finanças CLI Python Typer Selenium Automation",
+    long_description=long_description,
     url="https://github.com/JoaoGuedes01/scraper_portal_financas",
     version=open('.bumpversion.cfg').readlines()[1].split('=')[1].strip(),  # Reads version from .bumpversion.cfg
     packages=find_packages(),
